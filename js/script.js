c955779dd1f95794c1d0b1e7998b2acf0ef4a941
anabel <anabel.glannes@gmail.com>
2022-07-11 16:23:39 -0400

    cambiar backround color al dar click en rateButton

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-const {createApp} = Vue
+const { createApp } = Vue;
 createApp({
-    data() {
-        return {
-            rate: '1',
-            ratingNumbers:['1', '2', '3', '4', '5'],
-            activated: false
+  data() {
+    return {
+      rate: '',
+      ratingNumbers: [
+        { number: "1", isActive: false },
+        { number: "2", isActive: false },
+        { number: "3", isActive: false },
+        { number: "4", isActive: false },
+        { number: "5", isActive: false },
+      ],
+      activeStyle: {
+        backgroundColor: "hsl(25, 97%, 53%)",
+        color: "hsl(0, 0%, 100%)",
+      },
+    };
+  },
+  methods: {
+    changeBackgroud(ratingNumbers, ratingNumber, event) {
+      ratingNumber.isActive = ratingNumber.isActive == true ? false : true;
+
+      for (let i = 0; i < ratingNumbers.length; i++) {
+        if (
+          ratingNumbers[i].isActive == true &&
+          ratingNumbers[i].number != ratingNumber.number
+        ) {
+          ratingNumbers[i].isActive = false;
         }
+      }
+      console.log(rate);
     },
-    methods: {
-        changeBackgroud(number){
-           this.li.backgroundColor = 'hsl(25, 97%, 53%);';
-        }
+    changeRate(){
+        rate = ratingNumber.number;
     }
-}).mount('#app');
+  },
+}).mount("#app");
